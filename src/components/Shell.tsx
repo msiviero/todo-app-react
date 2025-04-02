ad3431ca89b1b497706a1e895932528c205a4160
Marco S <m.siviero83@gmail.com>
2025-04-02 16:46:24 +0200

    ui draft

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,5 +1,7 @@
 import { PropsWithChildren } from "react";
 
+// TODO https://medium.com/@dimterion/modals-with-html-dialog-element-in-javascript-and-react-fb23c885d62e
+// TODO https://react.dev/learn/extracting-state-logic-into-a-reducer
 export const Shell: React.FC<PropsWithChildren> = ({ children }) => {
     return (
         <div className="container mx-auto px-4">
