227d97013fc5b4546d44aaf242a3f08b61ebac64
Marco S <marco.siviero@doctolib.com>
2025-04-18 17:26:49 +0200

    uikit for presentation

--- src/TodoApp.tsx
+++ src/TodoApp.tsx
@@ -1,101 +1,68 @@
-import { CheckCircleOutlined, DeleteOutlined, DownOutlined, EditOutlined } from '@ant-design/icons';
-import { App, Layout, Typography, Table, Button, Dropdown, Space, ConfigProvider, theme, Flex } from 'antd';
-import type { MenuProps, TableColumnsType } from 'antd';
-import React from 'react';
-
-const { Header, Content } = Layout;
-const { Title } = Typography;
-
-const titleStyle = {
-    color: '#fff',
-}
-
-const contentStyle = {
-    margin: '0 auto',
-    padding: '24px 64px',
-    width: '100%',
-    maxWidth: '800px',
-};
-
-interface TodoEl {
-    key: React.Key;
-    title: string;
-    isCompleted: boolean;
-}
-
-const data: TodoEl[] = [
-    {
-        key: 1,
-        title: 'Learn React',
-        isCompleted: false,
-    },
-    {
-        key: 2,
-        title: 'Learn TypeScript',
-        isCompleted: true,
-    },
-];
-
-const items: MenuProps['items'] = [
-    {
-        icon: <CheckCircleOutlined />,
-        label: <a>Toggle complete</a>,
-        key: '0',
-    },
-    {
-        icon: <EditOutlined />,
-        label: <a>Edit</a>,
-        key: '1',
-    },
-    {
-        icon: <DeleteOutlined />,
-        label: <a>Delete</a>,
-        key: '2',
-    },
-];
-
-const columns: TableColumnsType<TodoEl> = [
-    {
-        title: 'Title',
-        dataIndex: 'title',
-        key: 'title',
-        render: (text: string, row: TodoEl) => (
-            <Flex justify='space-between'>
-                <span style={{ textDecoration: row.isCompleted ? 'line-through' : '' }}>{text}</span>
-                <Dropdown menu={{ items }} trigger={['click']}>
-                    <a onClick={(e) => e.preventDefault()}>
-                        <Space>
-                            Actions
-                            <DownOutlined />
-                        </Space>
-                    </a>
-                </Dropdown>
-            </Flex>
-        ),
-        defaultSortOrder: 'ascend',
-        sorter: (a, b) => a.title.localeCompare(b.title),
-        onFilter: (value, record) => record.isCompleted === value,
-        filters: [{
-            text: 'Show uncompleted only',
-            value: false,
-        }],
-    },
-];
 
 export const TodoApp: React.FC = () => (
-    <ConfigProvider theme={{ algorithm: [theme.defaultAlgorithm] }}>
-        <App>
-            <Layout style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-                <Header>
-                    <Title style={titleStyle}>Todo App</Title>
-                </Header>
-                <Content style={contentStyle}>
-                    <Table<TodoEl> dataSource={data} columns={columns} pagination={false} showSorterTooltip={{ target: 'sorter-icon' }} />
-                    <div style={{ padding: '24px 0' }}>
-                        <Button type="primary">Create</Button>
+    <>
+        <nav className="uk-navbar-container">
+            <div className="uk-container">
+                <div uk-navbar="true" className="uk-flex-between">
+                    <a className="uk-navbar-item uk-logo" href="/" aria-label="Back to Home">TodoApp</a>
+                    <div className="uk-navbar-item">
+                        <button className="uk-button uk-button-default uk-button-primary" uk-toggle="target:#modal-add-todo">
+                            <span uk-icon="plus-circle" className="uk-margin-small-right" />
+                            Add one
+                        </button>
                     </div>
-                </Content>
-            </Layout>
-        </App>
-    </ConfigProvider>
+                </div>
+            </div>
+        </nav>
+
+        <main>
+            <div className="uk-container">
+                <div className="uk-padding">
+                    <div className="uk-card uk-card-default uk-card-body">
+                        <h2 className="uk-card-title uk-heading-small">
+                            Buy the milk
+                        </h2>
+                        <p uk-margin="true">
+                            <button className="uk-button uk-button-primary uk-margin-small-right" >
+                                <span uk-icon="check" className="uk-margin-small-right" />
+                                Toggle done
+                            </button>
+                            {/* <button className="uk-button uk-button-default uk-margin-small-right">
+                                <span uk-icon="file-edit" className="uk-margin-small-right" />
+                                Edit
+                            </button> */}
+                            <button className="uk-button uk-button-danger uk-margin-small-right">
+                                <span uk-icon="minus-circle" className="uk-margin-small-right" />
+                                Delete
+                            </button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <div id="modal-add-todo" uk-modal="true">
+            <div className="uk-modal-dialog uk-modal-body">
+                <button className="uk-modal-close-default" type="button" uk-close="true"></button>
+                <h2 className="uk-modal-title">Add a todo</h2>
+                <div>
+                    <form>
+                        <fieldset className="uk-fieldset">
+                            <div className="uk-margin">
+                                <input className="uk-input" type="text" placeholder="Input" aria-label="Input" />
+                            </div>
+                            <div className="uk-margin">
+                                <p className="uk-text-right">
+                                    <button className="uk-button uk-button-primary" type="button">
+                                        <span uk-icon="check" className="uk-margin-small-right" />
+                                        Save
+                                    </button>
+                                </p>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </>
 )
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { TodoApp } from './TodoApp.tsx'
-import '@ant-design/v5-patch-for-react-19';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
